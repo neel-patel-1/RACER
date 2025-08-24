b0c8626db9f72f0ef6218601b8211c2c842943a2
Neel Maulik Patel <nmp83@urbana.ece.cornell.edu>
2025-08-24 14:15:58 -0400

    memfill/cpy en/dis cache control

--- include/dsa_offloads.h
+++ include/dsa_offloads.h
@@ -20,9 +20,17 @@ void free_dsa_wq();
 void prepare_dsa_memcpy_desc_with_preallocated_comp(
   struct hw_desc *hw, uint64_t src,
   uint64_t dst, uint64_t comp, uint64_t xfer_size);
+void prepare_dsa_memcpy_desc_with_preallocated_comp(
+  struct hw_desc *hw, uint64_t src,
+  uint64_t dst, bool cache_control_en,
+  uint64_t comp, uint64_t xfer_size);
 void prepare_dsa_memfill_desc_with_preallocated_comp(
   struct hw_desc *hw, uint64_t src,
   uint64_t dst, uint64_t comp, uint64_t xfer_size);
+void prepare_dsa_memfill_desc_with_preallocated_comp(
+  struct hw_desc *hw, uint64_t src,
+  uint64_t dst, bool cache_control_en,
+  uint64_t comp, uint64_t xfer_size);
 
 
 #endif
